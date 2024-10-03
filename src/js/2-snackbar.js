299ff018b858437a0f2d3f740c562e0b4818ab30
Hrebynakha Anatolii <hrebynakha@gmail.com>
2024-10-04 00:33:57 +0300

    add hw1

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,2 +1,41 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+
+const refs = {
+  btnSubmit: document.querySelector('.form button'),
+  form: document.querySelector('.form'),
+};
+
+refs.form.addEventListener('submit', hadleFromSnackSubmit);
+
+function hadleFromSnackSubmit(e) {
+  e.preventDefault();
+  const form = e.target;
+  const state = form.state.value;
+  const delay = form.delay.value;
+  setTimeout(() => {
+    const promise = createNewPromise(state, delay);
+    promise
+      .then(value => displaySuccess(value))
+      .catch(err => displayError(err));
+  }, Number(delay));
+  form.reset();
+}
+
+function createNewPromise(state, delay) {
+  return state === 'fulfilled' ? Promise.resolve(delay) : Promise.reject(delay);
+}
+
+function displaySuccess(delay) {
+  iziToast.success({
+    message: `Fulfilled promise in ${delay}ms`,
+    position: 'topRight',
+  });
+}
+
+function displayError(delay) {
+  iziToast.error({
+    message: `Rejected promise in ${delay}ms`,
+    position: 'topRight',
+  });
+}
